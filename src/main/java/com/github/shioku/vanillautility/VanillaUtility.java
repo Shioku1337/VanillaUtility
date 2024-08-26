a740b731fa91fe1ff5d635c04a451e2ddb5640ec
Jonas Müller <shioku1337@gmail.com>
2024-08-26 17:46:38 +0200

    moved scoreboardhandling

--- src/main/java/com/github/shioku/vanillautility/VanillaUtility.java
+++ src/main/java/com/github/shioku/vanillautility/VanillaUtility.java
@@ -1,13 +1,15 @@
 package com.github.shioku.vanillautility;
 
 import com.github.shioku.vanillautility.cmds.ChunkLoaderCmd;
-import com.github.shioku.vanillautility.cmds.ScoreboardPlaytimeCmd;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Statistic;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.*;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -15,8 +17,6 @@ import java.util.UUID;
 public final class VanillaUtility extends JavaPlugin {
 
   public static String PREFIX = formatColors("&8[&3Admin&8] &7");
-
-  public static List<UUID> ACTIVE_PLAYERS = new ArrayList<>();
 
   @Override
   @SuppressWarnings("ConstantConditions deprecation")
@@ -32,13 +32,35 @@ public final class VanillaUtility extends JavaPlugin {
 
     getCommand("chunkloader").setExecutor(new ChunkLoaderCmd(this));
     getCommand("chunkloader").setTabCompleter(new ChunkLoaderCmd(this));
-    getCommand("scoreboardplaytime").setExecutor(new ScoreboardPlaytimeCmd(this));
-    getCommand("scoreboardplaytime").setTabCompleter(new ScoreboardPlaytimeCmd(this));
+
+    registerScoreboardPlaytime();
   }
 
   @Override
   public void onDisable() {
     // Plugin shutdown logic
+  }
+
+  private void registerScoreboardPlaytime() {
+    ScoreboardManager manager = Bukkit.getScoreboardManager();
+
+    assert manager != null : "Scoreboard manager is null";
+
+    Scoreboard scoreboard = manager.getNewScoreboard();
+
+    Objective objective = scoreboard.registerNewObjective("playtime", Criteria.statistic(Statistic.PLAY_ONE_MINUTE), "Playtime");
+
+    objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+    objective.setDisplayName("Playtime");
+
+    Bukkit.getScheduler().runTaskTimer(this, task -> {
+      List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+      players.forEach(player -> {
+        Score score = objective.getScore(player);
+        score.setScore(player.getStatistic(Statistic.PLAY_ONE_MINUTE));
+        player.sendMessage("Your Statistic has been updated.");
+      });
+    }, 0, 60 * 20);
   }
 
   public static String formatColors(String arg) {
