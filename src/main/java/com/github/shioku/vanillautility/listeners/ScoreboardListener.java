16a5ba94678e7ef33f9c683595a83361254882bd
Jonas Müller <shioku1337@gmail.com>
2024-08-30 08:20:09 +0200

    Extracted code from onPlayerDamage and onRegainHealth

--- src/main/java/com/github/shioku/vanillautility/listeners/ScoreboardListener.java
+++ src/main/java/com/github/shioku/vanillautility/listeners/ScoreboardListener.java
@@ -6,8 +6,8 @@ import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -46,23 +46,15 @@ public class ScoreboardListener implements Listener {
 
   @EventHandler
   public void onPlayerDamage(EntityDamageEvent event) {
-    // Maybe only need to set for event player as in onJoin?
-    if (!this.plugin.isEnableHealth()) return;
-    if (!(event.getEntity() instanceof Player player)) return;
-
-    Objective obj = this.plugin.getScoreboard().getObjective("health");
-
-    assert obj != null : "Health objective is null";
-
-    double finalHealth = player.getHealth() - event.getFinalDamage();
-
-    if (finalHealth < 0) finalHealth = 0;
-
-    obj.getScore(player.getName()).setScore(Math.round((float) finalHealth));
+    this.handleHealth(event, event.getFinalDamage() * -1);
   }
 
   @EventHandler
   public void onRegainHealth(EntityRegainHealthEvent event) {
+    handleHealth(event, event.getAmount());
+  }
+
+  private void handleHealth(EntityEvent event, double healthDiff) {
     if (!this.plugin.isEnableHealth()) return;
     if (!(event.getEntity() instanceof Player player)) return;
 
@@ -70,9 +62,10 @@ public class ScoreboardListener implements Listener {
 
     assert obj != null : "Health objective is null";
 
-    double finalHealth = player.getHealth() + event.getAmount();
+    double finalHealth = player.getHealth() + healthDiff;
 
     if (finalHealth > 20.0) finalHealth = 20.0;
+    if (finalHealth < 0) finalHealth = 0;
 
     obj.getScore(player.getName()).setScore(Math.round((float) finalHealth));
   }
