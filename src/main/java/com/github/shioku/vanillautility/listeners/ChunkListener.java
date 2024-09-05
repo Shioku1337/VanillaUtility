2f47a20520affb7df43f25a0b26ce6b63cb621f1
Jonas Müller <shioku1337@gmail.com>
2024-09-05 09:31:55 +0200

    Fix - Chunk-Ticket persisting

--- src/main/java/com/github/shioku/vanillautility/listeners/ChunkListener.java
+++ src/main/java/com/github/shioku/vanillautility/listeners/ChunkListener.java
@@ -3,8 +3,10 @@ package com.github.shioku.vanillautility.listeners;
 import com.github.shioku.vanillautility.VanillaUtility;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Chunk;
+import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,26 +20,51 @@ public class ChunkListener implements Listener {
 
   @EventHandler
   public void onWorldLoad(WorldLoadEvent event) {
-    plugin.setChunkConfig(YamlConfiguration.loadConfiguration(plugin.getChunkFile()));
-    Chunk[] chunks = plugin.getChunkConfig().getObject("chunks." + event.getWorld().getUID(), Chunk[].class, new Chunk[0]);
+    World world = event.getWorld();
+
+    List<String> chunkXZ = plugin.getChunkConfig().getStringList("chunks." + world.getUID());
+
+    Chunk[] chunks = new Chunk[chunkXZ.size()];
+
+    for (int i = 0; i < chunks.length; i++) {
+      int x;
+      int z;
+
+      try {
+        x = Integer.parseInt(chunkXZ.get(i).split(",")[0]);
+        z = Integer.parseInt(chunkXZ.get(i).split(",")[1]);
+      } catch (NumberFormatException e) {
+        throw new RuntimeException("Could not parse chunk coordinates from chunks.yml", e);
+      }
+
+      chunks[i] = world.getChunkAt(x, z);
+    }
 
     for (Chunk chunk : chunks) {
-      if (!event.getWorld().getUID().equals(chunk.getWorld().getUID())) continue;
-
-      chunk.addPluginChunkTicket(this.plugin);
+      if (!chunk.getPluginChunkTickets().contains(plugin)) {
+        chunk.addPluginChunkTicket(plugin);
+      }
     }
   }
 
   @EventHandler
   public void onWorldUnload(WorldUnloadEvent event) {
-    Chunk[] chunks = new ArrayList<>(event.getWorld().getPluginChunkTickets().get(this.plugin)).toArray(new Chunk[0]);
+    World world = event.getWorld();
 
-    plugin.getChunkConfig().set("chunks." + event.getWorld().getUID(), chunks);
+    List<String> chunkXZ = new ArrayList<>();
+
+    for (Chunk chunk : world.getPluginChunkTickets().get(plugin)) {
+      chunkXZ.add(chunk.getX() + "," + chunk.getZ());
+    }
+
+    plugin.getChunkConfig().set("chunks." + world.getUID(), chunkXZ);
 
     try {
       plugin.getChunkConfig().save(plugin.getChunkFile());
     } catch (IOException e) {
-      plugin.getLogger().severe("Failed to save chunks config file.");
+      throw new RuntimeException(e);
     }
+
+    plugin.setChunkConfig(YamlConfiguration.loadConfiguration(plugin.getChunkFile()));
   }
 }
