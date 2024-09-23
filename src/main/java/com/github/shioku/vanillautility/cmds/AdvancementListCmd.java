6fc21ae9bbdd873ea9f0fa55e41dc16fe9b2444a
Jonas Müller <shioku1337@gmail.com>
2024-09-23 21:56:34 +0200

    implemented grep function

--- src/main/java/com/github/shioku/vanillautility/cmds/AdvancementListCmd.java
+++ src/main/java/com/github/shioku/vanillautility/cmds/AdvancementListCmd.java
@@ -1,6 +1,7 @@
 package com.github.shioku.vanillautility.cmds;
 
 import static com.github.shioku.vanillautility.VanillaUtility.PREFIX;
+import static com.github.shioku.vanillautility.misc.StringUtil.mm;
 
 import com.github.shioku.vanillautility.VanillaUtility;
 import com.github.shioku.vanillautility.misc.ComponentUtil;
@@ -12,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TranslatableComponent;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.advancement.Advancement;
@@ -31,13 +31,12 @@ public class AdvancementListCmd implements TabExecutor {
 
   private final VanillaUtility plugin;
 
-  private final Component infoComponent = MiniMessage.miniMessage()
-    .deserialize(
-      PREFIX +
-      "You can find the needed IDs on the " +
-      "<u><aqua><click:open_url:https://minecraft.fandom.com/wiki/Advancement#List_of_advancements>Minecraft Advancement List</click></aqua></u>! " +
-      "The column \"Resource Location\" is the correct input!"
-    );
+  private final Component infoComponent = mm(
+    PREFIX +
+    "You can find the needed IDs on the " +
+    "<u><aqua><click:open_url:https://minecraft.fandom.com/wiki/Advancement#List_of_advancements>Minecraft Advancement List</click></aqua></u>! " +
+    "The column \"Resource Location\" is the correct input!"
+  );
 
   @Override
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
@@ -47,9 +46,8 @@ public class AdvancementListCmd implements TabExecutor {
     }
 
     Audience audience = plugin.adventure().player(player);
-    MiniMessage miniMessage = MiniMessage.miniMessage();
 
-    if (args.length != 1) {
+    if (args.length != 1 && args.length != 2) {
       audience.sendMessage(StringUtil.getSyntaxError(cmd));
       audience.sendMessage(this.infoComponent);
       return true;
@@ -58,7 +56,7 @@ public class AdvancementListCmd implements TabExecutor {
     Advancement advancement = Bukkit.getAdvancement(NamespacedKey.minecraft(args[0]));
 
     if (advancement == null) {
-      audience.sendMessage(miniMessage.deserialize(PREFIX + "Syntaxerror, wrong advancement key! Please use: <red>" + cmd.getUsage()));
+      audience.sendMessage(mm(PREFIX + "Syntaxerror, wrong advancement key! Please use: <red>" + cmd.getUsage()));
       audience.sendMessage(this.infoComponent);
       return true;
     }
@@ -68,19 +66,31 @@ public class AdvancementListCmd implements TabExecutor {
     List<String> remainingCriteria = new ArrayList<>(advancementProgress.getRemainingCriteria());
 
     if (advancementProgress.isDone() || remainingCriteria.isEmpty()) {
-      audience.sendMessage(miniMessage.deserialize(PREFIX + "<green>Congratulations! <gray>You have already finished this advancement!"));
+      audience.sendMessage(mm(PREFIX + "<green>Congratulations! <gray>You have already finished this advancement!"));
       return true;
     }
 
     audience.sendMessage(
-      miniMessage
-        .deserialize(PREFIX + "The following progress is still missing from the advancement: \"<green>")
-        .append(advancementTitle)
-        .append(miniMessage.deserialize("<gray>\""))
+      mm(PREFIX + "The following progress is still missing from the advancement: \"<green>").append(advancementTitle).append(mm("<gray>\""))
     );
 
+    int amount = 0; // needed if grep is used
+
     for (String criteria : remainingCriteria) {
-      audience.sendMessage(miniMessage.deserialize(PREFIX + "Criteria: <gold>" + criteria.replace("minecraft:", "") + "</gold>"));
+      if (args.length == 2) {
+        if (criteria.contains(args[1])) {
+          audience.sendMessage(mm(PREFIX + "Criteria: <gold>" + criteria.replace("minecraft:", "") + "</gold>"));
+          amount++;
+        }
+      } else {
+        amount = -1;
+        audience.sendMessage(mm(PREFIX + "Criteria: <gold>" + criteria.replace("minecraft:", "") + "</gold>"));
+      }
+    }
+
+    if (amount == 0) {
+      audience.sendMessage(mm(PREFIX + "There were <red>no criteria found <gray>when filtering using \"<gold>" + args[1] + "<gray>\"."));
+      audience.sendMessage(mm(PREFIX + "Please check your filter or remove it entirely."));
     }
 
     return true;
@@ -88,19 +98,23 @@ public class AdvancementListCmd implements TabExecutor {
 
   @Override
   public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
-    if (!(sender instanceof Player player) || args.length != 1) return List.of();
+    if (!(sender instanceof Player player) || (args.length != 1 && args.length != 2)) return List.of();
 
-    Iterator<Advancement> advancementIterator = Bukkit.advancementIterator();
+    if (args.length == 1) {
+      Iterator<Advancement> advancementIterator = Bukkit.advancementIterator();
 
-    List<String> allAdvKeys = new ArrayList<>();
+      List<String> allAdvKeys = new ArrayList<>();
 
-    while (advancementIterator.hasNext()) {
-      Advancement adv = advancementIterator.next();
-      if (player.getAdvancementProgress(adv).isDone()) continue;
-      if (adv.getKey().getKey().startsWith("recipe")) continue;
-      allAdvKeys.add(adv.getKey().getKey());
+      while (advancementIterator.hasNext()) {
+        Advancement adv = advancementIterator.next();
+        if (player.getAdvancementProgress(adv).isDone()) continue;
+        if (adv.getKey().getKey().startsWith("recipe")) continue;
+        allAdvKeys.add(adv.getKey().getKey());
+      }
+
+      return allAdvKeys.stream().filter(str -> str.contains(args[0])).toList();
     }
 
-    return allAdvKeys.stream().filter(str -> str.contains(args[0])).toList();
+    return List.of("<filter-keyword>");
   }
 }
